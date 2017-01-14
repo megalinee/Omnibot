a84408d1fdac1d278c6cef5b273a5c38d9af2c9e
Jooshhg <punkgraphics@gmail.com>
2017-01-15 09:40:18 +1300

    Bot login

--- src/bot/main.js
+++ src/bot/main.js
@@ -5,4 +5,15 @@
 */
 
 const Discord = require('discord.js'); // IMPORT DISCORD
-const bot = require('discord.js');
+const bot = new Discord.Client(); // BOT OBJECT
+
+const fs = require('fs'); // IMPORT FS
+const colour = require('colors') // COLOURED CONSOLE IS 🔥
+
+const devConfig = JSON.parse(fs.readFileSync('../../../omnibot_config.json')); // CONFIG FOR PRIVATE STUFF
+
+/*
+	BOT LOGIN
+*/
+
+bot.login(devConfig.token); // LOGIN
