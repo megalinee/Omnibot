fdbe52fa595594840a2f02da628863440d045767
Murad <agariomadman@gmail.com>
2017-01-15 15:00:41 -0600

    Added userinfo command

--- src/bot/commands/commands.js
+++ src/bot/commands/commands.js
@@ -8,7 +8,8 @@ var commands = {
 
 	ping: require('./command_ping.js'),
 	say: require('./command_say.js'),
-	prefix: require('./command_prefix.js')
+	prefix: require('./command_prefix.js'),
+	userinfo: require('./command_userinfo.js')
 
 }
 
