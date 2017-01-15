b88a8def2bca0d4197e478b118f237bba70f08a4
Jooshhg <punkgraphics@gmail.com>
2017-01-16 12:31:53 +1300

    Command handler and alias working

--- src/bot/commands/command_ping.js
+++ src/bot/commands/command_ping.js
@@ -10,24 +10,28 @@ const command = {
 	description: 'A simple ping command to test the bot is working / get the ping to the server',
 	arguments: [],
 	requirement: 'none',
+	alias: ['test', 'piiing'],
 	process: (bot , message , config) => {
 
 		const now = require('performance-now');
 
-		const start = now();
+		const startTime = now();
 
 		message.channel.sendMessage('Getting `ping`')
 			.then(message => {
-				const end = now();
+				const endTime = now();
 
 				message.delete();
 
 				message.channel.sendEmbed({
 					color: 0x656a7d,
-					title: `Pong! ${(endTime - startTime).toFixed(3)}ms`
+					title: `Pong! ${(endTime - startTime).toFixed(0)}ms`
 				});
+
 			});
 
 	}
 
 }
+
+module.exports = command;
--- src/bot/commands/commands.js
+++ src/bot/commands/commands.js
@@ -7,9 +7,9 @@
 var commands = {
 
 	ping: require('./command_ping.js'),
-	say: require('./command_say.js'),
-	prefix: require('./command_prefix.js'),
-	userinfo: require('./command_userinfo.js')
+	//say: require('./command_say.js'),
+	//prefix: require('./command_prefix.js'),
+	//userinfo: require('./command_userinfo.js')
 
 }
 
